84352832012b66a476f12d65a7ce7062e793b256
David Yauri <david.yauri@tecsup.edu.pe>
2021-11-26 05:15:38 +0100

    first commit

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'database-1.cd1ibsvgxx2h.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'cgnj9fpr1',
+        database: 'db_links'
+    }
+
+};
